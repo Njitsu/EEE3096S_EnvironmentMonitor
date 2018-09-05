43d85b09660ed8b689ee99f3b588b52a4ab1eaec
Njitsu <43004629+Njitsu@users.noreply.github.com>
2018-09-05 16:16:43 +0200

    Cleanup

--- OutlineCode.py
+++ OutlineCode.py
@@ -23,8 +23,8 @@ GPIO.setup(display, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 channel = 0
 delay = 0.5
 
-def re(channel1):														#functions that are called when the event/interrupt occurs
-    print("re")
+def re(channel1):													   	   #functions that are called
+    print("re")                                                            #when the event/interrupt occurs
 
 def freq(channel):
     print("freq")
@@ -35,7 +35,7 @@ def stop(channel):
 def disp(channel):
     print("disp")
 
-def GetData(channel):											#ADC data collection function
+def GetData(channel):											           #ADC data collection function
     adc = spi.xfer2([1,(8+channel)<<4,0])
     data = ((adc[1]&3) << 8) + adc[2]
     return data
@@ -45,8 +45,8 @@ def ConvertVolts(data, places):
     volts = round(volts,places)
     return volts
 
-try:                                          #try/except conditional for ADC data collection.Event additions can go above this i think
-    while True:
+try:                                                                       #try/except conditional for ADC data collection.
+    while True:                                                            #Event additions can go above this i think
         sensor_data = GetData(channel)
         sensor_volt = ConvertVolts(sensor_data,2)
         time.sleep(delay)
@@ -54,7 +54,7 @@ try:                                          #try/except conditional for ADC da
 except KeyboardInterrupt:
     spi.close()
 
-print(sensor_volt)                              #testing ADC/ADC code
+print(sensor_volt)                                                          #testing ADC/ADC code
 GPIO.add_event_detect(reset, GPIO.RISING, callback=re, bouncetime=200)		#Event detector initialization
 
 GPIO.add_event_detect(frequency, GPIO.RISING, callback=freq, bouncetime=200)
@@ -64,7 +64,7 @@ GPIO.add_event_detect(stopbtn, GPIO.RISING, callback=stop, bouncetime=200)
 GPIO.add_event_detect(display, GPIO.RISING, callback=disp, bouncetime=200)
 
 try:
-    GPIO.wait_for_edge(reset, GPIO.FALLING)     #I think this try/except condition can go
+    GPIO.wait_for_edge(reset, GPIO.FALLING)                                 #I think this try/except condition can go
 
 except KeyboardInterrupt:
     GPIO.cleanup()
