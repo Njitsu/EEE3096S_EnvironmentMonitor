a0f1a3c0bc61f27199d307d575e892ee2796b82d
Njitsu <43004629+Njitsu@users.noreply.github.com>
2018-09-16 15:54:57 +0200

    Get ADC working to sense environment.

--- OutlineCode.py
+++ OutlineCode.py
@@ -3,24 +3,28 @@
 import RPi.GPIO as GPIO
 import spidev
 import os
-import sys
 import time
 
 spi = spidev.SpiDev()
 spi.open(0,0)
+spi.max_speed_hz = 1000000
+
 GPIO.setmode(GPIO.BCM)
 
-reset = 21
-frequency = 4
-stopbtn = 17
+#reset = 21
+#frequency = 4
+#stopbtn = 17
 display = 27
+#monitor = False
 
-
-GPIO.setup(reset, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(frequency, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(stopbtn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#GPIO.setup(reset, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#GPIO.setup(frequency, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#GPIO.setup(stopbtn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(display, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-channel = 0
+
+potChan = 0
+tempChan = 1
+ldrChan = 2
 delay = 0.5
 
 def re(channel1):													   	   #functions that are called
@@ -33,7 +37,7 @@ def stop(channel):
     print("stop")
 
 def disp(channel):
-    print("disp")
+    print("Volts: ", pot, "temp: ", temp, "light: ", light)
 
 def GetData(channel):											           #ADC data collection function
     adc = spi.xfer2([1,(8+channel)<<4,0])
@@ -45,27 +49,23 @@ def ConvertVolts(data, places):
     volts = round(volts,places)
     return volts
 
-try:                                                                       #try/except conditional for ADC data collection.
-    while True:                                                            #Event additions can go above this i think
-        sensor_data = GetData(channel)
-        sensor_volt = ConvertVolts(sensor_data,2)
-        time.sleep(delay)
+#GPIO.add_event_detect(reset, GPIO.RISING, callback=re, bouncetime=200)		
 
-except KeyboardInterrupt:
-    spi.close()
+#GPIO.add_event_detect(frequency, GPIO.RISING, callback=freq, bouncetime=200)
 
-print(sensor_volt)                                                          #testing ADC/ADC code
-GPIO.add_event_detect(reset, GPIO.RISING, callback=re, bouncetime=200)		#Event detector initialization
-
-GPIO.add_event_detect(frequency, GPIO.RISING, callback=freq, bouncetime=200)
-
-GPIO.add_event_detect(stopbtn, GPIO.RISING, callback=stop, bouncetime=200)
+#GPIO.add_event_detect(stopbtn, GPIO.RISING, callback=stop, bouncetime=200)
 
 GPIO.add_event_detect(display, GPIO.RISING, callback=disp, bouncetime=200)
 
-try:
-    GPIO.wait_for_edge(reset, GPIO.FALLING)                                 #I think this try/except condition can go
+try:                                                                       #try/except conditional for ADC data collection.
+    while True:                                                            #Event additions can go above this i think
+        pot = ConvertVolts(GetData(potChan), 1)
+        temp = (ConvertVolts(GetData(tempChan), 2)-0.5)*100
+        light = ConvertVolts(GetData(ldrChan), 2)*50
+        time.sleep(delay)
 
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    spi.close()                                                         
+
+
 GPIO.cleanup()
